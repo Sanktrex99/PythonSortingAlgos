36ee37c59f6f6d52b14a8d63dd8096c3a75de71c
Sanket <47188621+Sanktrex99@users.noreply.github.com>
2021-01-20 20:33:12 +0530

    Add files via upload
    My Sorting Commits

---
+++ QuickSort.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 20 20:16:21 2021
+
+@author: HP
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 20 20:12:09 2021
+
+@author: HP
+"""
+from random import randint
+
+def quicksort(array):
+    # If the input array contains fewer than two elements,
+    # then return it as the result of the function
+    if len(array) < 2:
+        return array
+
+    low, same, high = [], [], []
+
+    # Select your `pivot` element randomly
+    pivot = array[randint(0, len(array) - 1)]
+
+    for item in array:
+        # Elements that are smaller than the `pivot` go to
+        # the `low` list. Elements that are larger than
+        # `pivot` go to the `high` list. Elements that are
+        # equal to `pivot` go to the `same` list.
+        if item < pivot:
+            low.append(item)
+        elif item == pivot:
+            same.append(item)
+        elif item > pivot:
+            high.append(item)
+
+    # The final result combines the sorted `low` list
+    # with the `same` list and the sorted `high` list
+    return quicksort(low) + same + quicksort(high)
+
+
+
+
+lst = []
+
+# number of elemetns as input
+n = int(input("Enter number of elements : "))
+
+# iterating till the range
+for i in range(0, n):
+    ele = int(input("Enter Element:"))
+
+    lst.append(ele)  # adding the element
+
+print("Your Array:", lst)
+
+
+sortedArray = quicksort(lst)
+
+
+
+print("The sorted Array is:", sortedArray)
